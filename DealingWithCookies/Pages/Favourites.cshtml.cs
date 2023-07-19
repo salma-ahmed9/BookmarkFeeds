6a24eb29878bb51b7f9f83a3d059a32249bc9893
salma-ahmed9 <93517375+salma-ahmed9@users.noreply.github.com>
2023-07-19 14:23:50 +0200

    code updated

--- DealingWithCookies/Pages/Favourites.cshtml.cs
+++ DealingWithCookies/Pages/Favourites.cshtml.cs
@@ -27,53 +27,6 @@ public class FavouritesModel : PageModel
             }
         }
     }
-    /*public async Task<IActionResult> OnPostGetAjaxRequest([FromBody] FavouriteFeed favouriteFeedObject)
-    {
-        string feedTitle = favouriteFeedObject.FeedTitle;
-        string link = favouriteFeedObject.FeedLink;
-        string favouriteFeed = Request.Cookies["favourites"];
-        string[] feeds = favouriteFeed.Split(',');
-        List<string> myFeeds = feeds.ToList();
-        List<FavouriteFeed> favouriteFeeds = new List<FavouriteFeed>();
-        bool isFavourite = true;
-        foreach (var feed in myFeeds)
-        {
-             FavouriteFeed feedObject = new FavouriteFeed();
-             feedObject.FeedTitle = feed.Substring(feed.IndexOf('|') + 1);
-             feedObject.FeedLink = feed.Substring(0, feed.IndexOf('|'));
-             favouriteFeeds.Add(feedObject);
-        }
-        FavouriteFeedList = favouriteFeeds;
-        foreach(var feed in feeds)
-        {
-            if (feed == (link+ "|" + feedTitle))
-            {
-                feeds = feeds.Where(val => val != feed).ToArray();
-                foreach (var item in FavouriteFeedList)
-                {
-                    if (item.FeedTitle == feedTitle && item.FeedLink == link)
-                    {
-                        item.Visible = false;
-                        isFavourite = false;
-                        break;
-                    }
-                }
-
-            }
-        }
-        favouriteFeed = string.Join(",", feeds);
-        Response.Cookies.Append("favourites", favouriteFeed, new CookieOptions
-        {
-            Secure = Request.IsHttps,
-            Path = "/",
-            IsEssential = true,
-            SameSite = SameSiteMode.None,
-            Domain = "localhost",
-            HttpOnly = false
-        });
-        var response = new { message = isFavourite.ToString() };
-        return new JsonResult(response);
-    }*/
 }
 public class FavouriteFeed
 {
